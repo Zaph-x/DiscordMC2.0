e9f0cea823b64a3ee97be439c8ce704b9e575b93
Zaph <mikkel@luceartfoto.dk>
2019-05-08 18:44:32 +0200

    Added a working swear filter

--- src/main/java/com/github/zaphx/discordbot/managers/AntiSwearManager.java
+++ src/main/java/com/github/zaphx/discordbot/managers/AntiSwearManager.java
@@ -90,38 +90,4 @@ public class AntiSwearManager {
         }
         return map;
     }
-
-/*
-    public void handleMessage(IMessage message) {
-        String handledMessage;
-
-        String url = "https://www.purgomalum.com/service/containsprofanity?text=";
-
-        handledMessage = message.getContent().replaceAll(RegexPattern.USER.toString(),"");
-        handledMessage = handledMessage.replaceAll(RegexPattern.ROLE.toString(),"");
-        handledMessage = handledMessage.replaceAll(RegexPattern.EMOTES.toString(),"");
-        handledMessage = handledMessage.replaceFirst("(ob!\\s)", "");
-        handledMessage = handledMessage.replaceFirst("assign", "");
-
-        try {
-            URL filter = new URL(url + URLEncoder.encode(handledMessage, "UTF-8"));
-            URLConnection connection = filter.openConnection();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String result;
-            while ((result = reader.readLine()) != null) {
-                boolean isSwear = Boolean.valueOf(result);
-
-                if (isSwear) {
-                    RequestBuffer.request(message::delete);
-                    RequestBuffer.request(() -> message.getAuthor().getOrCreatePMChannel().sendMessage(embedManager.swearEmbed()));
-                    break;
-                }
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }*/
-
-
 }
